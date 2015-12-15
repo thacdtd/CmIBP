c8078e8d1c66a1010702e4814d1cf369df72dc65
Thac <thacdtd@gmail.com>
2015-12-15 14:18:34 +1100

    first commit

---
+++ IRM/irm.py
@@ -0,0 +1,15 @@
+__author__ = 'thac'
+
+import numpy as np
+
+class IRM:
+    def __init__(self):
+        pass
+
+    def run_irm(self):
+        a = "abc"
+        print a
+
+if __name__ == '__main__':
+    irm = IRM()
+    irm.run_irm()
